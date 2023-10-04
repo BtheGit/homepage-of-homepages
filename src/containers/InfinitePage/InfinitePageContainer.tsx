d687fdfa4cd13b3e3ec36b05627bf728410743eb
Brendan Beltz <brendanwebdev@gmail.com>
2023-10-04 19:37:48 -0400

    Skeleton inifinite page testing ground

---
+++ src/containers/InfinitePage/InfinitePageContainer.tsx
@@ -0,0 +1,9 @@
+export type InfinitePageContainerProps = {};
+
+export const InfinitePageContainer = (props: InfinitePageContainerProps) => {
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  );
+};
